a5beef4d8ddc1ab74d7c8a07707d879ff97e0503
Commit 1 <drm16g@my.fsu.edu>
2018-08-23 10:34:32 -0400

    Welcome screen & Adding Profile features

--- interface.py
+++ interface.py
@@ -3,58 +3,105 @@ from PyQt5 import QtGui as QtG
 from PyQt5 import QtWidgets as QtW
 import sys
 import os
+import selenium
 
 
-class addProfile(QtW.QWidget):
-	def __init__(self):
-		QtW.QWidget.__init__(self)
 
-	def view(self):
-		pass
 
 class NikeBot(QtW.QMainWindow):
 	
 	def __init__(self):
 		QtW.QMainWindow.__init__(self)
+
 		self.setFixedSize(800,600)
-		self.setWindowTitle('Sneaker Bot')
-
-
-
-
+		
 		addProfile = QtW.QAction('New profile',self)
 		addProfile.setStatusTip('Create a new profile')
 		addProfile.triggered.connect(self.addProfile)
-		
-
+	
+		viewProfiles = QtW.QAction('View Profiles',self)
+		viewProfiles.triggered.connect(self.viewProfiles)
 
 		menu_bar = self.menuBar()
 		menu_bar.setNativeMenuBar(False)
+		menu_bar.setStyleSheet('Color: RGB(0,0,0)')
+
 		profile_menu = menu_bar.addMenu('Profiles')
 		profile_menu.addAction(addProfile)
-	#	self.Image = DrawImage(self)
+		profile_menu.addAction(viewProfiles)
+		self.WelcomeWindow()
+
+	def WelcomeWindow(self):
+
+
+		self.setWindowTitle('Sneaker Bot')
+		Welcome = QtW.QLabel()
+		Welcome.setText('Welcome to sneaker bot')
+
+		
+		WelcomeWidget = QtW.QWidget()
+		layout = QtW.QVBoxLayout(WelcomeWidget)
+		l1 = QtW.QLabel()
+		l1.setText('Welcome to Sneaker Bot')
+		l1.setAlignment(QtC.Qt.AlignHCenter )
+#		WelcomeWidget.addWidget(l1)
+		image = QtW.QLabel()
+		
+		image.setPixmap(QtG.QPixmap('.gitignore/Sneaker_bot.png'))
+		layout.addWidget(l1)	
+		layout.addWidget(image)
+		#WelcomeWidget.addWidget(image)
+		self.setCentralWidget(WelcomeWidget)
+
+		
+			#	self.Image = DrawImage(self)
 		self.show()
 
 
 
 
 	def addProfile(self):
-		self.newprofile = QtW.QGroupBox('Enter all fields:')
+		newprofile = QtW.QGroupBox('Enter all text fields:')
 		layout = QtW.QFormLayout()		
 		layout.addRow( QtW.QLabel('Name:'), QtW.QLineEdit() )
 		layout.addRow( QtW.QLabel('Shipping Address:'), QtW.QLineEdit() )
 		layout.addRow( QtW.QLabel('Billing Address:'), QtW.QLineEdit() )
 		layout.addRow( QtW.QLabel('Credit Card #:'), QtW.QLineEdit() )	
-		layout.addRow( QtW.QPushButton('Save', self), QtW.QPushButton( 'Cancel', self))
+		save = QtW.QPushButton('Save', self)
+		cancel = QtW.QPushButton( 'Cancel', self)
+		layout.setFormAlignment(QtC.Qt.AlignHCenter | QtC.Qt.AlignVCenter ) 
+		layout.setSpacing(20)
+		layout.setLabelAlignment(QtC.Qt.AlignLeft)
+	
+		save.clicked.connect(self.saveProfile)
+		cancel.clicked.connect(self.WelcomeWindow)
+		layout.addRow( save, cancel )	
 
-		self.newprofile.setLayout(layout)
-		self.setCentralWidget(self.newprofile)
+		newprofile.setLayout(layout)
+		self.setCentralWidget(newprofile)
 		self.setWindowTitle('Create a new Bot')
 		self.show()
+
+
+	def viewProfiles(self):
+		self.setWindowTitle('Current user profiles')
+		self.setCentralWidget(None)
+		self.show()
 	
+	
+
+
+	def saveProfile(self):
+		file_selection = QtW.QInputDialog(self)
+		file_selection.setInputMode(QtW.QInputDialog.TextInput)
+		file_selection.show()
+
+
+
 def main():
 	app = QtW.QApplication(sys.argv)
 	main_window = NikeBot()
 	app.exec_()
+
 
 if __name__ == '__main__': main()	
